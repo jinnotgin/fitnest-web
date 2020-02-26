6bf3b1aeebf2870db63cdef0d6b5dcc4bd177e71
Jin <me@thelinjin.com>
2020-02-27 00:32:04 +0800

    Updated search to include facilities

--- src/routes/api/locations/[slug].json.js
+++ src/routes/api/locations/[slug].json.js
@@ -9,7 +9,7 @@ const getMongoDb = async () => {
   const connectionString = `mongodb://${mongodb_host}:${mongodb_port}`;
   return await (async () => {
     let client = await MongoClient.connect(connectionString, {
-      useNewUrlParser: true
+      useNewUrlParser: true,
     });
 
     return [client, client.db(db_name)];
@@ -34,24 +34,50 @@ export async function get(req, res, next) {
 
   const processedData = await (async () => {
     try {
-      const findOptions = { building: { $regex: `.*${slug.toLowerCase()}.*` } };
-      const fieldsToReturn = { building: 1, lat: 1, lng: 1, _id: 0 };
+      const processed = {};
 
-      const result = await db
+      // find from buildings
+      const buildings_findOptions = {
+        building: { $regex: `.*${slug.toLowerCase()}.*` },
+      };
+      const buildings_fieldsToReturn = { building: 1, lat: 1, lng: 1, _id: 0 };
+
+      const buildings_result = await db
         .collection("buildings")
-        .find(findOptions)
-        .project(fieldsToReturn)
+        .find(buildings_findOptions)
+        .project(buildings_fieldsToReturn)
         .limit(30)
         .toArray();
 
-      const processed = {};
-      result.map(({ building, lat, lng }) => {
+      buildings_result.map(({ building, lat, lng }) => {
         processed[
           _.startCase(_.toLower(building))
             .replace("Mrt", "MRT")
             .replace("Lrt", "LRT")
         ] = [lat, lng];
       });
+
+      // find from facilities
+      const facilities_findOptions = {
+        name: { $regex: `.*${slug.toLowerCase()}.*`, $options: "i" },
+      };
+      const facilities_fieldsToReturn = { name: 1, loc: 1, _id: 0 };
+
+      const facilities_result = await db
+        .collection("facilities")
+        .find(facilities_findOptions)
+        .project(facilities_fieldsToReturn)
+        .limit(30)
+        .toArray();
+
+      facilities_result.map(({ name, loc }) => {
+        console.log(name);
+        processed[_.startCase(_.toLower(name))] = [
+          loc.coordinates[1],
+          loc.coordinates[0],
+        ];
+      });
+
       return processed;
     } finally {
       client.close();
